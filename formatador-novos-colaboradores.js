f403881ff9188a41f3a855edfe711a113d73cbb8
PedroPiuma <luisppiuma@gmail.com>
2022-09-19 09:44:17 -0300

    add conector name (Da, De or length === 2) in lastName

--- formatador-novos-colaboradores.js
+++ formatador-novos-colaboradores.js
@@ -6,7 +6,7 @@ let htmlTextInfos = ''
 const writeHtml = () => writeFile('conteudo-final.html', htmlTextFinal.trim(), error => error ? new Error(error) : console.log('Script executado com sucesso! Verifique o arquivo conteudo-final.html :)\n'))
 const readCsv = formatText => readFile('novos-colaboradores.csv', 'utf-8', (error, text) => error ? new Error(error) : formatText(text))
 const createPhoto = (firstName, lastName) => `<td width="150"><img style="width: 120px; height: 160px; display: block; margin-left: auto; margin-right: auto; border-radius: 10%; border: 1px solid; border-color: #ab9b6a;" src="https://arquivos.essentialnutrition.com.br/images/novos-colaboradores/${firstName.normalize('NFD').replace(/[\u0300-\u036f]/g, "")}${lastName.normalize('NFD').replace(/[\u0300-\u036f]/g, "")}.jpg" alt="" /></td>`
-const createPersonDescription = (firstName, lastName, sector, local) => `<td width="150"><p style="text-align: center; font-family: arial, sans-serif; color: #1a1a1a; font-size: 12px; line-height: 18px; margin-bottom: 30px;"><b>${firstName} ${lastName}<br /></b>${sector}<br/>${local}</p></td>`
+const createPersonDescription = (firstName, lastName, sector, local) => `<td width="150"><p style="text-align: center; font-family: arial, sans-serif; color: #1a1a1a; font-size: 12px; line-height: 18px; margin: 15px 0 30px;"><b>${firstName} ${lastName}<br /></b>${sector}<br/>${local}</p></td>`
 const upperCaseFormatterName = text => text[0]?.toUpperCase() + text.slice(1)
 const upperCaseFormatterLocal = text => text.length <= 5 ? text.toUpperCase() : text.split(' ').map(word => word.length > 3 ? upperCaseFormatterName(word.toLowerCase()) : word.toUpperCase()).join(' ')
 const upperCaseFormatterSector = text => text.length <= 3 ? text : text.split(' ').map(word => upperCaseFormatterName(word.toLowerCase())).join(' ')
@@ -14,8 +14,10 @@ const formatText = text => {
     const objectPerson = text.split(',,\r\n').map(person => person.split(',').filter(el => el !== ''))
     const createRows = () => objectPerson.map((el, index) => {
         const [person, sector, local] = el
-        const firstName = upperCaseFormatterName(person.split(' ').filter(word => word !== '').shift().toLowerCase())
-        const lastName = upperCaseFormatterName(person.split(' ').filter(word => word !== '').pop().toLowerCase())
+        const names = person.split(' ').filter(word => word !== '')
+        const firstName = upperCaseFormatterName(names[0].toLowerCase())
+        const lastName = upperCaseFormatterName(names[names.length - 1].toLowerCase())
+        const conectorName = names[names.length - 2].length === 2 ? names[names.length - 2] : false
         const localName = upperCaseFormatterSector(local)
         const sectorName = upperCaseFormatterLocal(sector)
         if (index === 0) {
@@ -23,7 +25,7 @@ const formatText = text => {
             htmlTextInfos += '<tr>'
         }
         htmlTextImages += createPhoto(firstName, lastName)
-        htmlTextInfos += createPersonDescription(firstName, lastName, localName, sectorName)
+        htmlTextInfos += createPersonDescription(firstName, conectorName ? `${upperCaseFormatterName(conectorName)} ${lastName}` : lastName, localName, sectorName)
         if ((index + 1) % 4 === 0 && index !== objectPerson.length - 1) {
             htmlTextImages += '</tr>|<tr>'
             htmlTextInfos += '</tr>|<tr>'
